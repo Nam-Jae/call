c17bdb99230fa1aba9561147e8ac2c94fab87168
NAMJAE <34273834+Nam-Jae@users.noreply.github.com>
2024-02-21 20:35:27 +0900

    commit

---
+++ controlCenter/src/main/java/ecall/domain/Dispatched.java
@@ -0,0 +1,16 @@
+package ecall.domain;
+
+import ecall.infra.AbstractEvent;
+import java.time.LocalDate;
+import java.util.*;
+import lombok.Data;
+
+@Data
+public class Dispatched extends AbstractEvent {
+
+    private Long id;
+    private String carId;
+    private String carType;
+    private String workerId;
+    private String dispatchTime;
+}
